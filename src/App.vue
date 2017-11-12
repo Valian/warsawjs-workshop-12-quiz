cf965a221c01c98c2cf4aa6e592aba7bd9796fb4
Valian <jakub.skalecki@gmail.com>
2017-11-13 00:35:35 +0100

    first UI version

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-toolbar color="primary">
+      <v-toolbar-title>Taboo</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>New game</v-btn>
+        <v-btn flat>Random card</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+      <v-content>
+        <v-container>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+    </main>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -11,13 +25,18 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+  @require '~vuetify/src/stylus/settings/_colors'
+
+  $theme := {
+    primary: $red.darken-3
+    accent: $red.accent-2
+    secondary: $yellow.lighten-1
+    info: $blue.lighten-1
+    warning: $amber.darken-2
+    error: $red.accent-4
+    success: $green.lighten-2
+  }
+
+  @import '~vuetify/src/stylus/main.styl';
 </style>
