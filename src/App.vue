9d4abd6638ed89a5897b6496b39b26a59f674848
Valian <jakub.skalecki@gmail.com>
2017-11-13 22:40:14 +0100

    colors and background

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="app">
-    <v-toolbar color="primary">
+    <v-toolbar color="primary" :dark="true">
       <v-toolbar-title>Taboo</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
@@ -8,14 +8,13 @@
         <v-btn flat>Random card</v-btn>
       </v-toolbar-items>
     </v-toolbar>
-    <main>
+    <main class="c-app">
       <v-content>
         <v-container>
           <router-view></router-view>
         </v-container>
       </v-content>
     </main>
-    <v-footer app></v-footer>
   </v-app>
 </template>
 
@@ -29,7 +28,7 @@ export default {
   @require '~vuetify/src/stylus/settings/_colors'
 
   $theme := {
-    primary: $red.darken-3
+    primary: $green.darken-1
     accent: $red.accent-2
     secondary: $yellow.lighten-1
     info: $blue.lighten-1
@@ -39,4 +38,7 @@ export default {
   }
 
   @import '~vuetify/src/stylus/main.styl';
+
+  .c-app
+    background: url('/static/background.jpg') repeat
 </style>
