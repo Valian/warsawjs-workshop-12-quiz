c26b6f62e809e7a64e5db74195225dc814009688
Valian <jakub.skalecki@gmail.com>
2017-11-18 23:21:42 +0100

    moved mixin from vendor to utils

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,48 @@ export function loadingWrapper (promise, { loadingVar, errorVar }) {
     .then(sideEffect(() => { this[loadingVar] = false; this[errorVar] = false }))
     .catch(exc => { this[loadingVar] = false; this[errorVar] = exc.toString(); throw exc })
 }
+
+/* plugin for automatically binding ${name}Error and ${name}Loading values to async methods
+ Usage:
+  export default {
+    loading: ['getFromApi'],
+    methods: {
+      getFromApi() {
+        return this.$store.dispatch('getFromAPI')  // function must return promise!
+      }
+    },
+  }
+
+ Now data contains getFromApiLoading and getFromApiError synced with last call of a method
+ */
+export const loadingMixin = {
+  beforeCreate () {
+    const loadingMethodsList = this.$options.loading
+    if (loadingMethodsList) {
+      const additionalData = {}
+      for (const k of loadingMethodsList) {
+        const method = this.$options.methods[k]
+        const loadingVar = `${k}Loading`
+        const errorVar = `${k}Error`
+        if (!method) {
+          throw new Error(`Not found method ${k}`)
+        }
+        additionalData[loadingVar] = false
+        additionalData[errorVar] = null
+        this.$options.methods[k] = function () {
+          const result = method.apply(this, arguments)
+          if (result && result.then) {
+            return loadingWrapper.call(this, result, { loadingVar, errorVar })
+          }
+          return result
+        }
+      }
+
+      const oldData = this.$options.data
+      this.$options.data = function () {
+        const data = oldData ? oldData.apply(this, arguments) : {}
+        return { ...additionalData, ...data }
+      }
+    }
+  }
+}
--- src/vendor.js
+++ src/vendor.js
@@ -1,53 +1,10 @@
 import Vue from 'vue'
 import Loading from './common/Loading.vue'
-import { loadingWrapper } from './utils'
+import { loadingMixin } from './utils'
 
 Vue.component('o-loading', Loading)
 
-/* plugin for automatically binding ${name}Error and ${name}Loading values to async methods
- Usage:
- export default {
-   loading: ['getFromApi']
-   methods: {
-     getFromApi() {
-       return this.$store.dispatch('getFromAPI')  // function must return promise!
-     }
-   },
- }
-
- Now data contains getFromApiLoading and getFromApiError synced with last call of a method
- */
-Vue.mixin({
-  beforeCreate () {
-    const loadingMethodsList = this.$options.loading
-    if (loadingMethodsList) {
-      const additionalData = {}
-      for (const k of loadingMethodsList) {
-        const method = this.$options.methods[k]
-        const loadingVar = `${k}Loading`
-        const errorVar = `${k}Error`
-        if (!method) {
-          throw new Error(`Not found method ${k}`)
-        }
-        additionalData[loadingVar] = false
-        additionalData[errorVar] = null
-        this.$options.methods[k] = function () {
-          const result = method.apply(this, arguments)
-          if (result && result.then) {
-            return loadingWrapper.call(this, result, { loadingVar, errorVar })
-          }
-          return result
-        }
-      }
-
-      const oldData = this.$options.data
-      this.$options.data = function () {
-        const data = oldData ? oldData.apply(this, arguments) : {}
-        return { ...additionalData, ...data }
-      }
-    }
-  }
-})
+Vue.mixin(loadingMixin)
 
 Vue.filter('currency', value => '$ ' + value.toString()
   .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
