59cadc4d4e789f2b7f9d7d4379cc9bfdf867cc89
Valian <jakub.skalecki@gmail.com>
2017-12-10 00:17:26 +0100

    checkpoint 3 - NarrowLayout, children routes

---
+++ src/common/NarrowLayout.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="columns">
+    <router-view class="column is-4 is-offset-4"></router-view>
+  </div>
+</template>
--- src/components/IntroView.vue
+++ src/components/IntroView.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="columns">
-    <div class="column is-offset-4 is-4 box has-text-centered">
+    <div class="box has-text-centered">
       <h1 class="title">Who Wants To Be a Milionaire?</h1>
       <div>
         <img src="/static/logo.png">
       </div>
       <router-link :to="{name: 'rules'}" class="button is-fullwidth">PLAY</router-link>
     </div>
-  </div>
 </template>
