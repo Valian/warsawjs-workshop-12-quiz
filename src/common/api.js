3285e2f96ebebc457bbf768614229848c1dc0033
Valian <jakub.skalecki@gmail.com>
2017-12-10 01:15:19 +0100

    mocked questions response

--- src/common/api.js
+++ src/common/api.js
@@ -1,8 +1,10 @@
 import shuffle from 'lodash/shuffle'
 import axios from 'axios'
+import mockResponse from './mockResponse.json'
 import { DIFFICULTY_LEVELS } from './const'
 
 const QUESTION_API_ENDPOINT = 'https://opentdb.com/api.php'
+const MOCK = true
 
 const mapApiQuestionToAppQuestion = q => {
   const answers = shuffle([
@@ -18,7 +20,13 @@ const mapApiQuestionToAppQuestion = q => {
   }
 }
 
-export const getQuestions = (amount, difficulty = DIFFICULTY_LEVELS.MEDIUM) =>
+const getQuestionsMock = (amount, difficulty = DIFFICULTY_LEVELS.MEDIUM) =>
+  Promise.resolve(mockResponse)
+    .then(data => data.results.map(mapApiQuestionToAppQuestion))
+
+const getQuestionsFromApi = (amount, difficulty = DIFFICULTY_LEVELS.MEDIUM) =>
   axios.get(QUESTION_API_ENDPOINT, {params: { amount, difficulty }})
     .then(response => response.data.results
       .map(mapApiQuestionToAppQuestion))
+
+export const getQuestions = MOCK ? getQuestionsMock : getQuestionsFromApi
